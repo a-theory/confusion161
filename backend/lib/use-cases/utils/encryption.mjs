fcf6149fc22742fa93cb4d87d3685ddce53cab7a
filatov-a <a.filatov.xxi@gamil.com>
2022-11-11 21:54:34 +0100

    fix

--- backend/lib/use-cases/utils/encryption.mjs
+++ backend/lib/use-cases/utils/encryption.mjs
@@ -1,11 +1,5 @@
 import aesjs from 'aes-js';
 
-const key_256 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-    16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-    29, 30, 31];
-
-const key_256_buffer = Buffer.from(key_256);
-
 export function encryptAES(text, key) {
     const textBytes = aesjs.utils.utf8.toBytes(text);
 
