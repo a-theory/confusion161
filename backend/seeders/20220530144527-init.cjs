79889f408dcb8ad6dfec9729ae2e5607d21bc5c2
filatov-a <a.filatov.xxi@gamil.com>
2022-12-05 16:45:14 +0100

    kubernetes done

--- backend/seeders/20220530144527-init.cjs
+++ backend/seeders/20220530144527-init.cjs
@@ -1,12 +1,22 @@
 const crypto = require('crypto');
+const { v4: uuidv4 } = require('uuid');
+const argon2 = require("argon2");
 
 module.exports = {
     up : async (queryInterface, Sequelize) => {
-        // await queryInterface.bulkInsert('', [
-        //
-        // ]);
+        const id = uuidv4();
+        const passwordHash = await argon2.hash("123456789");
+        await queryInterface.bulkInsert('Users', [{
+            id: id,
+            name: "Artem",
+            password: passwordHash,
+            email: "a.filatov.xxi@gmail.com",
+            status: "VERIFIED",
+            createdAt: new Date(),
+            updatedAt: new Date()
+    }]);
     },
     down : async (queryInterface, Sequelize) => {
-        // await queryInterface.bulkDelete('', null, {});
+        await queryInterface.bulkDelete('Users', null, {});
     }
 };
